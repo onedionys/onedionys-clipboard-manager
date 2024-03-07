a594065f71667982802c94512b3ccb24c47d8a23
onedionys <onedionys@gmail.com>
2024-03-07 22:26:21 +0700

    chore: adding test package before publish

---
+++ test/index.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const ClipboardManager = require('../src/clipboardManager');
+
+describe('ClipboardManager', () => {
+    describe('#copy()', () => {
+        it('should copy text to clipboard', () => {
+            // Write a test here
+            // Contoh: assert.equal(ClipboardManager.copy('test'), true);
+        });
+    });
+
+    describe('#paste()', () => {
+        it('should return text from clipboard', () => {
+            // Write a test here
+            // Contoh: assert.equal(ClipboardManager.paste(), 'test');
+        });
+    });
+});
