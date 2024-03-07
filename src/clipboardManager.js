96e1c054f8ad932ae6d4f1965b11eadccbe9a2e4
onedionys <onedionys@gmail.com>
2024-03-07 22:26:17 +0700

    chore: initialize function project

---
+++ src/clipboardManager.js
@@ -0,0 +1,11 @@
+class ClipboardManager {
+    static copy(text) {
+        // Implementasi untuk menyalin teks ke clipboard
+    }
+
+    static paste() {
+        // Implementasi untuk mengambil teks dari clipboard
+    }
+}
+
+module.exports = ClipboardManager;
